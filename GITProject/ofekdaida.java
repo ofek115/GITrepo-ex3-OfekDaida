2604f33b0a02cc85eca6470f12502f0a536f43d9
ofek115 <157103475+ofek115@users.noreply.github.com>
2024-02-02 02:29:35 +0200

    initial commit

---
+++ GITProject/ofekdaida.java
@@ -0,0 +1,10 @@
+public class Hello
+{
+    public static void main(String[] args) 
+    {
+        for (int i=1;i<=9; i++) 
+        {    
+            System.out.println("ofek daida ..."+i);   
+        }  
+    }
+} 
